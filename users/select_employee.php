0bf9af4452213cc56f6049cc27a80a998e216459
owenpanusorn <85387068+owenpanusorn@users.noreply.github.com>
2021-08-07 00:36:58 +0700

    update

--- users/select_employee.php
+++ users/select_employee.php
@@ -247,14 +247,23 @@ try {
     <section class="bg-light showbarber">
         <div class="container p-0">
             <div class="row">
-                <div class="col-6 col-md-3 me-2">
+            <?php                        
+                        $result = $db->prepare('SELECT * from tb_employee');  
+                        $result->execute();
+                        
+                        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        
+                  ?> 
+                <div class="col-12 col-md-3 mb-1">
                     <a href="detail_emp.php?uu_id=<?php echo $uuid ?>">
                         <div class="card" style="width: 18rem;">
 
-                            <img src="img/barber2.jpg" alt="" class="card-img-top">
+                        <?php echo '<img src="../Admin/images/employee/'.$row["images"].'" class="card-img-top" height=250">'?>
+                            <!-- <img src="../Admin/images/" alt="" class="card-img-top"> -->
 
                             <div class="card-body">
-                                <h5 class="card-title text-center"><?php echo $fname ?></h5>
+                                <h5 class="card-title text-center"><?php echo $row["fname"] ?></h5>
+
                                 <p class="text-warning text-center card-text">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                         class="bi bi-star-fill" viewBox="0 0 16 16">
@@ -288,46 +297,7 @@ try {
                         </div>
                     </a>
                 </div>
-
-                <div class="col-6 col-md-3 ">
-                    <a href="#">
-                        <div class="card " style="width: 18rem;">
-
-                            <img src="img/barber2.jpg" alt="" class="card-img-top">
-
-                            <div class="card-body">
-                                <h5 class="card-title text-center">Barber 1</h5>
-                                <p class="text-warning text-center card-text">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                        class="bi bi-star-fill" viewBox="0 0 16 16">
-                                        <path
-                                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                                    </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                        class="bi bi-star-fill" viewBox="0 0 16 16">
-                                        <path
-                                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                                    </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                        class="bi bi-star-fill" viewBox="0 0 16 16">
-                                        <path
-                                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                                    </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                        class="bi bi-star-fill" viewBox="0 0 16 16">
-                                        <path
-                                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                                    </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
-                                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
-                                      </svg>
-                                </p>
-                                <p class="kanitB text-center mb-1 fw-bold card-text">( 4.0 คะแนน)</p>
-                                <h5 class="kanitB text-center text-success fw-bolder">ว่าง</h5>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+                <?php } ?>
             </div>
         </div>
         </div>
